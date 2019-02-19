777bc15d5299ebc506da418975c15ca42a196c88
Bogdan Tudos <bogdantudos@gmail.com>
2019-02-19 10:39:20 -0800

    Updates

---
+++ precache-manifest.74c6e9a5fbeb7191c7d51a50ff9adcac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7040145a5f910ab68310",
+    "url": "/tmo-vegetation-detection/static/js/runtime~main.7040145a.js"
+  },
+  {
+    "revision": "07835f0290cf6e67bfb7",
+    "url": "/tmo-vegetation-detection/static/js/main.07835f02.chunk.js"
+  },
+  {
+    "revision": "30f4c6e1bcebec935cdb",
+    "url": "/tmo-vegetation-detection/static/js/2.30f4c6e1.chunk.js"
+  },
+  {
+    "revision": "07835f0290cf6e67bfb7",
+    "url": "/tmo-vegetation-detection/static/css/main.c44be7ec.chunk.css"
+  },
+  {
+    "revision": "aaffce8bfab707c9812d14806cbf2554",
+    "url": "/tmo-vegetation-detection/index.html"
+  }
+];
